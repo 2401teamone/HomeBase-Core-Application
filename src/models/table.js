1a882605685d999767663f9585973f8379b8c4ed
trees543 <76708281+trees543@users.noreply.github.com>
2024-03-28 13:56:51 -0500

    Fixed comments in migrationDao
    Co-authored-by: Alan Cho <alancho397@gmail.com>
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>
    Co-authored-by: Josh Lakenan <joshualakenan@gmail.com>

--- src/models/table.js
+++ src/models/table.js
@@ -252,8 +252,8 @@ class Table {
     });
 
     const migrateTemplate = `
-      // import { MigrationDao } from "pinniped";
-      import { MigrationDao } from "../../src/pinniped/pinniped.js";
+      import { MigrationDao } from "pinniped";
+      // import { MigrationDao } from "../../src/pinniped/pinniped.js";
 
       export async function up(knex) {
         const dao = new MigrationDao(knex);
@@ -292,8 +292,8 @@ class Table {
     });
 
     const migrateTemplate = `
-        // import { MigrationDao } from "pinniped";
-        import { MigrationDao } from "../../src/pinniped/pinniped.js";
+        import { MigrationDao } from "pinniped";
+        // import { MigrationDao } from "../../src/pinniped/pinniped.js";
 
         export async function up(knex) {
           const dao = new MigrationDao(knex);
@@ -345,8 +345,8 @@ class Table {
     });
 
     const migrateTemplate = `
-    // import { MigrationDao } from "pinniped";
-    import { MigrationDao } from "../../src/pinniped/pinniped.js";
+    import { MigrationDao } from "pinniped";
+    // import { MigrationDao } from "../../src/pinniped/pinniped.js";
 
     export async function up(knex) {
       const oldTable = ${JSON.stringify(this)};
