8e6b9e08bbcbb895f7caf2d9263b9b547180a7b9
Josh Lakenan <joshualakenan@gmail.com>
2024-05-07 14:05:22 -0500

    Set up connect-session-knex session store for testing
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>

--- src/api/middleware/session_config.js
+++ src/api/middleware/session_config.js
@@ -1,15 +1,32 @@
 import session from "express-session";
-import store from "better-sqlite3-session-store";
-import sqlite from "better-sqlite3";
+// import store from "better-sqlite3-session-store";
+// import sqlite from "better-sqlite3";
 import fs from "fs";
+import knex from "knex";
+import connectSessionKnex from "connect-session-knex";
+
 import {
   createSessionSecret,
   saveSessionSecret,
 } from "../../utils/create_session_secret.js";
 
-const SqliteStore = store(session);
+// const SqliteStore = store(session);
 if (!fs.existsSync("pnpd_data")) fs.mkdirSync("pnpd_data");
-const db = new sqlite("pnpd_data/session.db");
+
+//creating a new knex connection with better-sqlite3 as the client
+let db = knex({
+  client: "better-sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "pnpd_data/pnpd.db",
+  },
+  pool: {
+    afterCreate: function (connection, done) {
+      connection.pragma("journal_mode = WAL");
+      done(false, connection);
+    },
+  },
+});
 
 if (!process.env.SESSION_SECRET) {
   const secret = createSessionSecret();
@@ -19,13 +36,18 @@ if (!process.env.SESSION_SECRET) {
 
 export default function sessionConfig() {
   return session({
-    store: new SqliteStore({
-      client: db,
-      expired: {
-        clear: true,
-        intervalMs: 900000, //ms = 15min
-      },
+    store: new (connectSessionKnex(session))({
+      knex: db,
+      clearInterval: 60000, // 1 min
     }),
+    //setting up session store
+    // store: new SqliteStore({
+    //   client: db,
+    //   expired: {
+    //     clear: true,
+    //     intervalMs: 900000, //ms = 15min
+    //   },
+    // }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
