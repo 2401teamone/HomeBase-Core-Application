9f6c42efc36f3d3ba4f9624ccf732fba1ff6bfe1
Jonathan Hurd <hurd.jonathan@gmail.com>
2024-06-20 16:57:35 -0500

    Cleanup comments

--- src/dao/log_dao.js
+++ src/dao/log_dao.js
@@ -1,46 +1,7 @@
-// import knex from "knex";
-// import generateRandomUuid from "../utils/generate_uuid.js";
 import fs from "fs";
 
-/**
- * Creates an Knex instance that connects to the better-sqlite3 'logs.db'.
- */
 class LogDao {
-  constructor() {
-    // this.db = knex({
-    //   client: "better-sqlite3",
-    //   useNullAsDefault: true,
-    //   connection: {
-    //     afterCreate: function (connection, done) {
-    //       connection.pragma("journal_mode = WAL");
-    //       done(false, connection);
-    //     },
-    //     filename: "pnpd_data/logs.db",
-    //   },
-    // });
-  }
-
-  /**
-   *  Creates the table 'logs' with its appropriate schema, following the structure of the custom logs structure.
-   */
-  // async createTable() {
-  //   const logsExists = await this.db.schema.hasTable("logs");
-
-  //   if (!logsExists) {
-  //     await this.db.schema.createTable("logs", (table) => {
-  //       table.specificType("id", "TEXT PRIMARY KEY");
-  //       table.integer("pid");
-  //       table.integer("level");
-  //       table.string("time");
-  //       table.string("hostname");
-  //       table.string("method");
-  //       table.string("url");
-  //       table.json("headers");
-  //       table.integer("statusCode");
-  //       table.integer("responseTime");
-  //     });
-  //   }
-  // }
+  constructor() {}
 
   async getLogs() {
     const data = fs.readFileSync("./pnpd_data/server.logs", "utf-8");
@@ -57,16 +18,6 @@ class LogDao {
     return objectsArray;
   }
 
-  // async getLogs() {
-  //   return this.db("logs").select("*");
-  // }
-
-  // async insertLog(log) {
-  //   return this.db("logs")
-  //     .insert(log)
-  //     .catch((err) => console.error(err));
-  // }
-
   async deleteLog(id) {
     console.log(id);
     const data = fs.readFileSync("./pnpd_data/server.logs", "utf-8");
@@ -75,29 +26,6 @@ class LogDao {
     lines.splice(logIndex, 1);
     fs.writeFileSync("./pnpd_data/server.logs", lines.join("\n"));
   }
-
-  /**
-   * @return {object}
-   * Within the returned object contains a write method that optionally writes to 'logs.db'
-   * On the condition that the request URL within the log is not from the '/api/admin/logs/ path or '/api/auth/' path.
-   * This object gets returned to the Pino instance which allows this method to run on every log.
-   */
-  // sqliteStream() {
-  //   return {
-  //     write: (log) => {
-  //       const parsedLog = this.parseLog(log);
-  //       if (
-  //         parsedLog.url.startsWith("/api/admin/logs") ||
-  //         parsedLog.url === "/api/auth/"
-  //       )
-  //         return;
-
-  //         // new TextEncoder().encode(JSON.stringify(parsedLog).length) / 1024
-  //       );
-  //       this.insertLog({ id: generateRandomUuid(), ...parsedLog });
-  //     },
-  //   };
-  // }
 
   /**
    * @param {obj} log
